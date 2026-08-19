fa277cb09209668cc573b0f5d9e0bbbb83118a00
Bulna007 <aquibk647@gmail.com>
2026-08-19 20:42:10 +0530

    Spring Cloud API Gateway

---
+++ Microservices/ecomm-api-gateway/src/main/java/org/bulna/ecommapigateway/EcommApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.bulna.ecommapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommApiGatewayApplication.class, args);
+    }
+
+}
